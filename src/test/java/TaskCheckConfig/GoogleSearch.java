a6c77e4a66156fdea45a0d22fde987cfb33e6095
mjozar <m.j.ozarowski@gmail.com>
2023-05-28 11:16:45 +0200

    zadanie3

---
+++ src/test/java/TaskCheckConfig/GoogleSearch.java
@@ -0,0 +1,22 @@
+package TaskCheckConfig;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearch {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.google.com");
+        driver.findElement(By.id("L2AGLb")).click();
+        WebElement element = driver.findElement(By.name("q"));
+        element.clear();
+        element.sendKeys("Coderslab");
+        element.submit();
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/TaskCheckConfig/main03.java
@@ -0,0 +1,23 @@
+package TaskCheckConfig;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class main03 {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(" https://www.google.com");
+        driver.get("https://coderslab.pl/pl");
+        driver.navigate().back();
+        driver.get("https://mystore-testlab.coderslab.pl/index.php");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+    }
+
+}
