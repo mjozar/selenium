ab28e278622bfe4cd5ccaed31bfac285badedfdf
mjozar <m.j.ozarowski@gmail.com>
2023-05-28 16:44:23 +0200

    wiciąganie tekstu

---
+++ src/test/java/TaskCheckConfig/getText.java
@@ -0,0 +1,17 @@
+package TaskCheckConfig;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class getText {
+    public static void main(String[] args) throws InterruptedException  {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://pl.wikipedia.org/");
+        WebElement element = driver.findElement(By.id("main-page-column1"));
+        System.out.println(((WebElement) element).getText());
+    }
+
+
+}
